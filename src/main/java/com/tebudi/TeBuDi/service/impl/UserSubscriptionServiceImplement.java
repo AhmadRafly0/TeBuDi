f82fa313b6e9c98047f7382ba1422fcd85c970f1
nerdjeez <fariz.speed333@gmail.com>
2026-04-13 01:11:13 +0700

    add fitur : generate langganan otomatis saat register dan sistem self-healing untuk user lama

--- src/main/java/com/tebudi/TeBuDi/service/impl/UserSubscriptionServiceImplement.java
+++ src/main/java/com/tebudi/TeBuDi/service/impl/UserSubscriptionServiceImplement.java
@@ -2,20 +2,25 @@ package com.tebudi.TeBuDi.service.impl;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tebudi.TeBuDi.dto.UserSubscriptionDTO;
 import com.tebudi.TeBuDi.model.UserSubscription;
+import com.tebudi.TeBuDi.model.User;
+import com.tebudi.TeBuDi.repository.UserRepository;
 import com.tebudi.TeBuDi.repository.UserSubscriptionRepository;
 import com.tebudi.TeBuDi.service.UserSubscriptionService;
 
+import lombok.RequiredArgsConstructor;
+
 
 @Service
+@RequiredArgsConstructor
 public class UserSubscriptionServiceImplement implements UserSubscriptionService{
-
-    @Autowired
-    public UserSubscriptionRepository userSubscriptionRepository;
+    
+    private final UserSubscriptionRepository userSubscriptionRepository;
+    private final UserRepository userRepository;
 
     @Override
     public boolean isUserSubscribed(String userId){
@@ -25,7 +30,7 @@ public class UserSubscriptionServiceImplement implements UserSubscriptionService
     @Override
     public UserSubscriptionDTO getSubscriptionDetails(String userId) {
         UserSubscription sub = userSubscriptionRepository.findByUserId(userId)
-            .orElseThrow(() -> new RuntimeException("Data langganan tidak ditemukan untuk user ini"));
+            .orElseGet(() -> createDefaultSubscription(userId));
 
         UserSubscriptionDTO dto = new UserSubscriptionDTO();
         dto.setId(sub.getId());
@@ -38,5 +43,18 @@ public class UserSubscriptionServiceImplement implements UserSubscriptionService
         dto.setActive(stillActive);
 
         return dto;
+    }
+
+    private UserSubscription createDefaultSubscription(String userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("Data langganan tidak ditemukan untuk user ini"));
+        
+        UserSubscription newSubs = new UserSubscription();
+        newSubs.setUser(user);
+        newSubs.setStartDate(LocalDateTime.now());
+        newSubs.setEndDate(LocalDateTime.now());
+        newSubs.setStatus(false);
+
+        return userSubscriptionRepository.save(newSubs);
     }
 }
